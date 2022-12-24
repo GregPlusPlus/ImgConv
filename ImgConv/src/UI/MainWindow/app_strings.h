a3f43fb0885de9c9980f01524149e71d66597ecd
gregoire <bostgregoire@gmail.com>
2022-12-24 13:08:30 +0100

    Added app_strings.h (omitted)

---
+++ ImgConv/src/UI/MainWindow/app_strings.h
@@ -0,0 +1,30 @@
+/**
+ ** This file is part of the ImgConv project.
+ ** Copyright 2022-2023 Grégoire BOST <bostgregoire@gmail.com>.
+ **
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as
+ ** published by the Free Software Foundation, either version 3 of the
+ ** License, or (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+#ifndef APP_STRINGS_H
+#define APP_STRINGS_H
+
+#define APP_NAME "Accelerated Image Convolution Filtering"
+#define COPYRIGHT "Copyright 2022-2023 Grégoire BOST <bostgregoire@gmail.com>"
+#define LGPL_STR "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version." \
+"<br>" \
+"This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details." \
+"<br>" \
+"You should have received a copy of the GNU General Public License along with this program. If not, see <a href='https://www.gnu.org/licenses/'>https://www.gnu.org/licenses/</a>."
+
+#endif // APP_STRINGS_H
