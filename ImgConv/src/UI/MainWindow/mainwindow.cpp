7b5ae58abaf3d3d7f67914bbfb7bb350ca2fc3f5
gregoire <bostgregoire@gmail.com>
2023-07-07 19:07:42 +0200

    #39 : Added cancel button for image correction

--- ImgConv/src/UI/MainWindow/mainwindow.cpp
+++ ImgConv/src/UI/MainWindow/mainwindow.cpp
@@ -173,7 +173,12 @@ void MainWindow::startImageCorrection(const QString &kernelPath) {
     m_runAction->setDisabled(true);
     m_selectDeviceAction->setDisabled(true);
 
-    m_waitDialogMgr.createWaitDialog(pid, tr("Correcting image..."), Dialogs::WaitDialog::Flags::ShowProgress);
+    Dialogs::WaitDialog *dialog = m_waitDialogMgr.createWaitDialog(pid, tr("Correcting image..."),
+                                                                    Dialogs::WaitDialog::Flags::ShowProgress |
+                                                                    Dialogs::WaitDialog::Flags::Cancelable);
+
+    connect(dialog, &Dialogs::WaitDialog::cancelProcess, m_coreApp->ocl(), &Core::OCLWrapper::requestKernelCancelation);
+    connect(m_coreApp->ocl(), &Core::OCLWrapper::kernelCancelationRequested, dialog, &Dialogs::WaitDialog::cancelProgressPending);
 }
 
 void MainWindow::openImage() {
